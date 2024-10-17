5cda26e9b361b91fd2f53e1ad751abe027d2f188
Marlon Prado <marlonprado04@gmail.com>
2024-10-17 18:14:13 -0300

    docs: added firs functions

---
+++ 01_CURSO_C_SHARP_CRIANDO_SUA_PRIMEIRA_APLICACAO/funcao.cs
@@ -0,0 +1,33 @@
+// Primeira função
+
+void ExibirMensagem()
+{
+    console.WriteLine(@"
+    
+██████╗░░█████╗░░█████╗░░██████╗  ██╗░░░██╗██╗███╗░░██╗██████╗░░█████╗░░██████╗██╗
+██╔══██╗██╔══██╗██╔══██╗██╔════╝  ██║░░░██║██║████╗░██║██╔══██╗██╔══██╗██╔════╝██║
+██████╦╝██║░░██║███████║╚█████╗░  ╚██╗░██╔╝██║██╔██╗██║██║░░██║███████║╚█████╗░██║
+██╔══██╗██║░░██║██╔══██║░╚═══██╗  ░╚████╔╝░██║██║╚████║██║░░██║██╔══██║░╚═══██╗╚═╝
+██████╦╝╚█████╔╝██║░░██║██████╔╝  ░░╚██╔╝░░██║██║░╚███║██████╔╝██║░░██║██████╔╝██╗
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░  ░░░╚═╝░░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝
+    
+    ");    
+   
+}
+
+
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Con
+}
+
+ExibirMensagem();
+ExibirOpcoesDoMenu();
+
